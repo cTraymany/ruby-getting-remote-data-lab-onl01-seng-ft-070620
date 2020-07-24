a44951306cdf7e8332e702f1da528274515515f6
Chindalath Traymany <chindalatht@gmail.com>
2020-07-24 05:17:04 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -7,15 +7,14 @@ class GetRequester
   def initialize(url)
     @url = url
   end
-
+  
   def get_response_body
-    uri = URI.parse(url)
-    response = Net::HTTP.get_response(uri)
+    uri = URI.parse(self.url)
+    response = Net::HTTP.get_response(self.uri)
     response.body
   end
-
+  
   def parse_json
-    parse_me = JSON.parse(self.get_response_body)
+    JSON.parse(self.get_response_body)
   end
-
 end
