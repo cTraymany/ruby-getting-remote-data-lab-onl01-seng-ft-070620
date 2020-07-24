71dea7afa6c3374fc35116c52cbfbdd550d9293c
Chindalath Traymany <chindalatht@gmail.com>
2020-07-24 05:14:46 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -17,23 +17,4 @@ class GetRequester
   def parse_json
     JSON.parse(self.get_response_body)
   end
-  
 end
-
-
-# class GetRequester
-
-#   def get_response_body
-#     uri = URI.parse(url)
-#     response = Net::HTTP.get_response(uri)
-#     response.body
-#   end
-
-#   def parse_json
-#     parse_me = JSON.parse(self.get_response_body)
-#   end
-
-
-
-# # url = https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json
-# end
